2488bb8559fa4b3f7d77ceb5bd8e2266d4a96a3f
Dennis Wanyoike Kuria <dwanyoike@codedcell.com>
2017-05-09 22:17:52 +0300

    Passport Authentication with Axios

--- app/src/renderer/components/Minute/Minute.vue
+++ app/src/renderer/components/Minute/Minute.vue
@@ -13,17 +13,22 @@
                 </div>
             </div>
         </div>
+
         <div class="view-pane" style="margin-top: 86px; height: 100%; padding-top: 6px;">
+
             <div class="ui  grid" style="height: 100%;">
+
                 <div class="ten wide column"
                      style=" border-right: 1px solid rgba(0,0,0,0.1); padding-right: 0px !important; padding-top: 0px !important;">
-                    <div class="toolbar" style="border-bottom: 1px solid rgba(0,0,0,0.1);padding: 2px; display: none;">
-                        <button class="mini ui icon blue button">
-                            <i class="cloud icon"></i>
+                    <div class="toolbar"
+                         style="text-align: center;border-bottom: 1px solid rgba(0,0,0,0.1);padding: 2px; ">
+                        <button class="mini ui icon blue button" v-on:click="getToken">
+                            <i class="cloud upload icon"></i> Upload Minutes
                         </button>
-                        <button class="mini ui icon purple button">
-                            <i class="file pdf 0utline icon"></i>
+                        <button v-on:click="updateApp" v-if="updatable" class="mini ui icon red button">
+                            <i class="cloud download icon"></i> Update Available !!
                         </button>
+
                     </div>
                     <div style="height: 100%; overflow-y: auto; padding-bottom: 80px;  ">
                         <div v-if="filename == null" style="margin: 10px;" class="ui blue message">
@@ -32,7 +37,7 @@
 
 
                         </div>
-                        <div style="margin: 10px; margin-bottom:-25px; cursor: pointer; " class="ui red message" v-on:click="updateApp" v-if="updatable"><i class="cloud download icon"></i> Update Available click to install</div>
+
 
                         <div class="ui divided items minute-item">
 
@@ -55,7 +60,7 @@
 
 
         </div>
-
+        <Token v-on:done="visible=false" :visible="visible"></Token>
     </div>
 
 </template>
@@ -66,95 +71,102 @@
     import Item from './MinuteItem.vue'
     import Agenda from '../Agenda/Agenda.vue'
     import Vue from 'vue'
+    import Token from '../Token/Token.vue'
     const ipc = require('electron').ipcRenderer
 
     export default {
-      props: ['saved_minutes', 'attendants', 'saved_agendas', 'filename','updatable'],
-      components: {
-        Agenda,
-        Item
-      },
-      mounted: function () {
+        props: ['saved_minutes', 'attendants', 'saved_agendas', 'filename', 'updatable'],
+        components: {
+            Agenda,
+            Item,
+            Token
+        },
+        mounted: function () {
 
-      },
-      data: function () {
-        return {
-          minutes: [],
-          minute: '',
-          tags: '',
-          modified_minute: '',
-          agenda: '',
-          agendas: [],
-          editing_minute: null,
-          insert_after: null
-        }
-      },
-      methods: {
-          updateApp:function(){
-              ipc.send('updateApplication');
-          },
-        addItemAfter: function (item) {
-          var index = this.minutes.indexOf(item)
-          this.insert_after = index + 1
-          this.$refs.minuteEditor.focus()
-          this.$toasted.info('You are inserting a minute press ESC to discard', {
-            theme: "bubble",
-            position: "bottom-center",
-            duration: 8000
-          })
         },
-        getTags: function findHashtags (searchText) {
-          var regexp = /\B\#\w\w+\b/g
-          var result = searchText.match(regexp)
-          if (result) {
-            return result
-          } else {
-            return false
-          }
-        },
-        checkIfIncomplete: function (searchText) {
-          var regexp = /\?{2}/g
-          var result = searchText.match(regexp)
-          console.log(result)
-          if (result == null) {
-            return false
-          } else {
-            this.minute = this.minute.replace(/\?{2}/g, '')
-            return true
-          }
-        },
-        getPeople: function findHashtags (searchText) {
-          var regexp = /\B\@\w\w+\b/g
-          var result = searchText.match(regexp)
-          var items = []
-          if (result == null) {
-            return []
-          }
-          for (var i = 0; i < result.length; i++) {
-            var item = result[i]
-            item = item.replace(/@(\w+)/g, '$1')
-            var x = this.attendants.findIndex(x => x.acronym == item)
-            if (x > -1) {
-              items.push(this.attendants[x].attendant)
+        data: function () {
+            return {
+                minutes: [],
+                minute: '',
+                tags: '',
+                modified_minute: '',
+                agenda: '',
+                agendas: [],
+                editing_minute: null,
+                insert_after: null,
+                visible: false
             }
-          }
-          if (result) {
-            return items
-          } else {
-            return false
-          }
         },
-        updateAgenda: function (agenda) {
-          this.agendas = agenda;
-          this.$emit('agenda', agenda)
-        },
-        cancelEdit: function () {
-          this.editing_minute = null;
-          this.insert_after = null;
-          this.minute = '';
-        },
-        editMinute: function (item) {
+        methods: {
+            getToken: function () {
+                this.visible = true;
+            },
+            updateApp: function () {
+                ipc.send('updateApplication');
+            },
+            addItemAfter: function (item) {
+                var index = this.minutes.indexOf(item)
+                this.insert_after = index + 1
+                this.$refs.minuteEditor.focus()
+                this.$toasted.info('You are inserting a minute press ESC to discard', {
+                    theme: "bubble",
+                    position: "bottom-center",
+                    duration: 8000
+                })
+            },
+            getTags: function findHashtags(searchText) {
+                var regexp = /\B\#\w\w+\b/g
+                var result = searchText.match(regexp)
+                if (result) {
+                    return result
+                } else {
+                    return false
+                }
+            },
+            checkIfIncomplete: function (searchText) {
+                var regexp = /\?{2}/g
+                var result = searchText.match(regexp)
+                console.log(result)
+                if (result == null) {
+                    return false
+                } else {
+                    this.minute = this.minute.replace(/\?{2}/g, '')
+                    return true
+                }
+            },
+            getPeople: function findHashtags(searchText) {
+                var regexp = /\B\@\w\w+\b/g
+                var result = searchText.match(regexp)
+                var items = []
+                if (result == null) {
+                    return []
+                }
+                for (var i = 0; i < result.length; i++) {
+                    var item = result[i]
+                    item = item.replace(/@(\w+)/g, '$1')
+                    var x = this.attendants.findIndex(x => x.acronym == item)
+                    if (x > -1) {
+                        items.push(this.attendants[x].attendant)
+                    }
+                }
+                if (result) {
+                    return items
+                } else {
+                    return false
+                }
+            },
+            updateAgenda: function (agenda) {
+                this.agendas = agenda;
+                this.$emit('agenda', agenda)
+            },
+            cancelEdit: function () {
+                this.editing_minute = null;
+                this.insert_after = null;
+                this.minute = '';
+            },
+            editMinute: function (item) {
 
+<<<<<<< Updated upstream
           this.$refs.minuteEditor.focus()
           this.$toasted.info('You are editing a minute press ESC key to discard', {
             theme: "bubble",
@@ -206,64 +218,117 @@
               position: "bottom-center",
               duration: 5000
             })
+=======
+                this.$refs.minuteEditor.focus()
+                this.$toasted.info('You are editing a minute press ESC key to discard', {
+                    theme: "bubble",
+                    position: "bottom-center",
+                    duration: 8000
+                })
+                this.editing_minute = this.minutes.indexOf(item)
+                this.minute = item.minute.trim()
+            },
+            substitutePeople: function (minute) {
+                for (var key in this.attendants) {
+                    var attendant = this.attendants[key].attendant
+                    var acronym = this.attendants[key].acronym
+                    var replace = '@' + acronym
+                    minute = minute.replace(replace, attendant)
+                }
+                return minute
+            },
+            getActiveAgenda: function () {
+                var x = this.agendas.findIndex(x => x.active == true)
+                if (x > -1) {
+                    this.agenda = this.agendas[x].text
+                }
+            },
+            addMinute: function () {
+                var incomplete = this.checkIfIncomplete(this.minute)
+                this.tags = this.getTags(this.minute)
+                this.people = this.getPeople(this.minute)
+                this.modified_minute = this.minute.replace(/#(\w+)/g, '$1')
+                this.modified_minute = this.modified_minute.replace(/^\@(\w+)/g, '@$1 :')
+                this.modified_minute = this.substitutePeople(this.modified_minute)
+                this.getActiveAgenda()
+                var minuteObject = {
+                    minute: this.minute.trim(),
+                    modified_minute: this.modified_minute,
+                    tags: this.tags,
+                    people: this.people,
+                    // attendants: this.attendants,
+                    agenda: this.agenda,
+                    date: moment().format('YYYY-M-D'),
+                    time: moment().format('h:mm'),
+                    incomplete: incomplete
+                }
+                if (this.editing_minute !== null) {
+                    this.minutes[this.editing_minute] = minuteObject
+                    this.editing_minute = null
+                    this.$toasted.success('Minute Updated', {
+                        theme: "bubble",
+                        position: "bottom-center",
+                        duration: 5000
+                    })
+>>>>>>> Stashed changes
 
-          } else if (this.insert_after !== null) {
-            this.minutes.splice(this.insert_after, 0, minuteObject);
-            this.insert_after = null
-          } else {
-            this.minutes.push(minuteObject)
-            this.editing_minute = null
-          }
+                } else if (this.insert_after !== null) {
+                    this.minutes.splice(this.insert_after, 0, minuteObject);
+                    this.insert_after = null
+                } else {
+                    this.minutes.push(minuteObject)
+                    this.editing_minute = null
+                }
 
-          this.minute = ''
+                this.minute = ''
+            },
+            deleteMinute: function (item) {
+                var index = this.minutes.indexOf(item)
+                if (index > -1) {
+                    this.minutes.splice(index, 1);
+                }
+            },
+            editLastMinute: function () {
+                this.editMinute(this.minutes.slice(-1)[0])
+            },
+            moveToNextAgenda: function () {
+                var x = this.agendas.findIndex(x => x.active == true)
+                var index = 0;
+                if (x > -1) {
+                    this.agendas[x].active = false
+                    index = x + 1
+                }
+                if (index > this.agendas.length - 1) {
+                    index = 0;
+                }
+                this.agendas[index].active = true
+                this.agenda = this.agendas[index].text
+                this.$toasted.error('You are now writing Minutes for agenda : ' + this.agenda, {
+                    theme: "bubble",
+                    position: "bottom-center",
+                    duration: 8000
+                })
+            }
         },
-        deleteMinute: function (item) {
-          var index = this.minutes.indexOf(item)
-          if (index > -1) {
-            this.minutes.splice(index, 1);
-          }
-        },
-        editLastMinute: function () {
-          this.editMinute(this.minutes.slice(-1)[0])
-        },
-        moveToNextAgenda: function () {
-          var x = this.agendas.findIndex(x => x.active == true)
-          var index = 0;
-          if (x > -1) {
-            this.agendas[x].active = false
-            index = x + 1
-          }
-          if (index > this.agendas.length - 1) {
-            index = 0;
-          }
-          this.agendas[index].active = true
-          this.agenda = this.agendas[index].text
-          this.$toasted.error('You are now writing Minutes for agenda : ' + this.agenda, {
-            theme: "bubble",
-            position: "bottom-center",
-            duration: 8000
-          })
-        }
-      },
-      watch: {
-        minutes: function () {
-          this.$emit('input', this.minutes)
-        },
-        agenda: function () {
+        watch: {
+            minutes: function () {
+                this.$emit('input', this.minutes)
+            },
+            agenda: function () {
 
+            },
+            saved_minutes: function () {
+                this.minutes = this.saved_minutes
+            },
+            saved_agendas: function () {
+                this.agendas = this.saved_agendas
+            }
         },
-        saved_minutes: function () {
-          this.minutes = this.saved_minutes
-        },
-        saved_agendas: function () {
-          this.agendas = this.saved_agendas
+        computed: {
+            ordered_mins: function () {
+                return this.minutes.slice().reverse();
+            }
         }
-      },
-      computed: {
-        ordered_mins: function () {
-          return this.minutes.slice().reverse();
-        }
-      }
     }
 </script>
 
